c73f5c1d106ef3d37422537930bd5d09805c62fe
Unknown <fabianrk@gmail.com>
2018-01-21 12:54:43 +0200

    new main 1

--- main.c
+++ main.c
@@ -30,33 +30,70 @@ TaskFeeder create_task_feeder(poolManager pool) {
 
 int main(int argc, const char * argv[]) {
     
-    int num_iterations,num_threads,num_tasks,interation_ptask,interation;
+  int num_iterations;
+    int num_threads;
+    int num_tasks;
+    int show_debug_messages;
+    int iterations_per_task;
+    int i;
     Task task;
     List tasks;
-    poolManager pool;
-    TaskFeeder task_feeder;
+    TaskArgs task_args;
+    Node temp;
+    TreadPoolManager manager;
+    TaskFeeder feeder;
 
-    num_iterations=atoi(argv[0]);
-    num_threads=atoi(argv[1]);
-    num_tasks=atoi(argv[2]);
-    num_iterations=num_iterations/num_tasks;
-    
-        
-    tasks=createList();
-    pool=createPool(num_threads);
-    task_feeder=create_task_feeder(pool);
-    
-    for(int i=0;i<num_tasks;i++){
-        interation = interation_ptask;
-    }    
-    
-    
-    printf("hello\n");
-    
-    
-    
-    
-    
-    
+    if (argc != 5) {
+        log_error("Usage: ./main NUM_ITERATIONS NUM_THREADS NUM_TASKS SHOW_DEBUG_MESSAGES");
+        exit(1);
+    }
+
+    num_iterations = strtol(argv[1], NULL, 10);
+    num_threads = atoi(argv[2]);
+    num_tasks = atoi(argv[3]);
+    show_debug_messages = atoi(argv[4]);
+    iterations_per_task = num_iterations / num_tasks;
+
+    log_init(show_debug_messages);
+
+    log_debug("num_iterations = %d", num_iterations);
+    log_debug("num_threads = %d", num_threads);
+    log_debug("num_tasks = %d", num_tasks);
+    log_debug("iterations_per_task = %d", iterations_per_task);
+
+    int reminder_iterations = num_iterations % num_tasks;
+    if (reminder_iterations != 0) {
+        log_info("Last task will be %d iterations because %d %% %d = %d"
+                , iterations_per_task + reminder_iterations
+                , num_iterations
+                , num_tasks
+                , reminder_iterations);
+    }
+
+    tasks = create_list();
+    manager = create_pool_manager(num_threads);
+    log_debug("Thread pool manager was created with %d threads", num_threads);
+    feeder = create_task_feeder(manager);
+    log_debug("Task feeder was created");
+
+    for (i = 0; i < num_tasks; i++) {
+        int iterations = iterations_per_task + (i == num_tasks - 1 ? reminder_iterations : 0);
+        task_args = create_task_args(iterations);
+        task = create_task(&monte_carlo_func, (void*) task_args);
+        log_debug("Task #%d (id: %p) was created with %d iterations", i + 1, task, iterations);
+        add_to_list(tasks, (void*) task);
+        feeder_load_task(feeder, task);
+    }
+
+    while (!all_tasks_finished(tasks)) {
+        usleep(100);
+    }
+
+
+    manager_stop_all_threads(manager);
+    log_debug("Stopped all threads");
+
+    log_info("PI estimation result is = %lf", estimate_pi(tasks));
+
     return 0;
 }
